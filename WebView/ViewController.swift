68f298a49936b464cbc40a51367be6fb1ffa7709
mzler <timo@fam-haberl.de>
2017-01-24 22:44:06 +0100

    a little iOS App to view html in an native app

--- WebView/ViewController.swift
+++ WebView/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var WebView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        WebView.loadRequest(NSURLRequest(url: NSURL(fileURLWithPath: Bundle.main.path(forResource: "HtmlCode", ofType: "html")!) as URL) as URLRequest)
+        
+     
     }
-
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
